1be623ad52fa7014314a3a6f997302fda488163f
cm-collins <dev-collins@outlook.com>
2026-06-05 11:28:45 +0300

    Initial Commit

---
+++ LoanApplication/LoanApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanApplicationApp.swift
+//  LoanApplication
+//
+//  Created by Victor Mutethia on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoanApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoanApplicationTests/LoanApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoanApplicationTests.swift
+//  LoanApplicationTests
+//
+//  Created by Victor Mutethia on 05/06/2026.
+//
+
+import Testing
+@testable import LoanApplication
+
+struct LoanApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
